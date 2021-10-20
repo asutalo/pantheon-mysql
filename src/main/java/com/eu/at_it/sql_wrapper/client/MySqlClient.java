cd5a9ff8d387d2237f17a68ae71cd8a9cf2b099f
asutalo <a.sutalo@hotmail.com>
2021-10-20 19:34:58 +0100

    todo...

--- src/main/java/com/eu/at_it/sql_wrapper/client/MySqlClient.java
+++ src/main/java/com/eu/at_it/sql_wrapper/client/MySqlClient.java
@@ -38,13 +38,18 @@ public class MySqlClient {
         return execute(queryBuilder, otherDmlQueryResultProcessorFunction);
     }
 
+    /**
+     * TODO add support for more complicated transactions by adding method that returns an open connection with
+     *  connection.setAutoCommit(false);
+     *  not sure if while autocommit is false a ResultSet can be created... might need to play with Savepoints as well...
+     *  in any case, should be doable to open a connection, execute multiple queries, and then close connection to persist
+     */
     <T> T execute(QueryBuilder queryBuilder, Function<PreparedStatement, T> preparedStatementExecutor) throws SQLException {
         Connection connection = connector.connect();
         PreparedStatement preparedStatement = queryBuilder.prepareStatement(connection);
         T queryResults = preparedStatementExecutor.apply(preparedStatement);
         preparedStatement.close();
         connector.close(connection);
-
         return queryResults;
     }
 }
